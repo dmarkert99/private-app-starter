8b29e57c213ea71016e53792c1447d0eda0f3d44
dmarkert99 <90805272+dmarkert99@users.noreply.github.com>
2025-03-05 10:25:53 +0100

    stuff

--- python/private_app_starter.py
+++ python/private_app_starter.py
@@ -1,18 +1,25 @@
 import os
 from dotenv import load_dotenv
 from hubspot import HubSpot
-from flask import Flask
+from flask import Flask, request
 
 # please, set FLASK_APP in your .env file
 app = Flask(__name__)
 
 # use home path
-@app.route("/")
-def get_contacts():
+@app.route("/hubspot/webhook", methods=['GET', 'POST'])
+def webhook():
   api_client = HubSpot()    # get hubspot client
   api_client.access_token = access_token()    # set your private app access token
-  contacts = api_client.crm.contacts.basic_api.get_page()    # get your contacts
-  return str(contacts)   # return your contacts as json
+  request_data = request.get_json()
+  print(request_data[0]["objectId"])
+  quote_data=api_client.crm.objects.basic_api.get_by_id("quotes", request_data[0]["objectId"])
+  if quote_data["prperties"]["hs_status"]=="DRAFT":
+    print("new Draft")
+    return("200")
+  print(quote_data)
+  return request_data   # return your contacts as json
+  
 
 # get access_token from .env file
 def access_token():
